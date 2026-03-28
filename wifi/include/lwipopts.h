9f5f25f0ae504c7fe72c1b9d5bd94018a25049bd
Mikael Wallin <mikael.x.wallin@gmail.com>
2026-03-28 17:10:18 +0100

    Added wifi

---
+++ wifi/include/lwipopts.h
@@ -0,0 +1,27 @@
+#ifndef _LWIPOPTS_H
+#define _LWIPOPTS_H
+
+#define NO_SYS                      1
+#define LWIP_SOCKET                 0
+#define LWIP_NETCONN                0
+#define MEM_ALIGNMENT               4
+#define MEM_SIZE                    4000
+#define MEMP_NUM_TCP_SEG            32
+#define MEMP_NUM_ARP_QUEUE          10
+#define PBUF_POOL_SIZE              24
+#define LWIP_ARP                    1
+#define LWIP_ETHERNET               1
+#define LWIP_ICMP                   1
+#define LWIP_RAW                    1
+#define LWIP_DHCP                   1
+#define LWIP_IPV4                   1
+#define LWIP_TCP                    1
+#define LWIP_UDP                    1
+#define LWIP_DNS                    1
+#define LWIP_TCP_KEEPALIVE          1
+#define LWIP_NETIF_STATUS_CALLBACK  1
+#define LWIP_NETIF_LINK_CALLBACK    1
+#define LWIP_NETIF_HOSTNAME         1
+#define LWIP_CALLBACK_API           1
+
+#endif /* _LWIPOPTS_H */
